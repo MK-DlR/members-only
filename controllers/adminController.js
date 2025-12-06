024d6f3d130cd358196d1d9a005ea0f2d705b31b
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-12-06 12:16:55 -0500

    feat: implement admin form code verification

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,47 +5,44 @@ const pool = require("../db/pool");
 
 // admin form route
 const adminGet = (req, res) => {
-  res.render("admin", { title: "Administrator" });
+  res.render("admin", { title: "Administrator Access" });
 };
 
 // admin handler
 const adminPost = async (req, res, next) => {
-  /*
   try {
     // success
     if (
-      req.body.code === process.env.MEMBERSHIP_CODE &&
+      req.body.admin_code === process.env.ADMIN_CODE &&
       req.isAuthenticated()
     ) {
-      // update membership status
-      await pool.query("UPDATE users SET membership = $1 WHERE id = $2", [
+      // update admin status
+      await pool.query("UPDATE users SET admin = $1 WHERE id = $2", [
         true,
         req.user.id,
       ]);
 
-      res.render("membership", {
-        title: "Membership",
-        success: [{ msg: "Membership successfully activated!" }],
+      res.render("admin", {
+        title: "Administrator Access",
+        success: [{ msg: "Administrator access successfully activated!" }],
       });
-    } else if (req.body.code != process.env.MEMBERSHIP_CODE) {
+    } else if (req.body.admin_code != process.env.ADMIN_CODE) {
       // failure, wrong code
-      res.render("membership", {
-        title: "Membership",
-        errors: [{ msg: "Not a valid membership code." }],
+      res.render("admin", {
+        title: "Administrator Access",
+        errors: [{ msg: "Not a valid administrator access code." }],
       });
     } else {
       // failure, not logged in
-      res.render("membership", {
-        title: "Membership",
+      res.render("admin", {
+        title: "Administrator Access",
         errors: [{ msg: "Please log in first." }],
       });
     }
   } catch (error) {
-    console.error("Membership error:", error);
+    console.error("Administrator access error:", error);
     next(error);
   }
-    */
-  console.log("placeholder");
 };
 
 module.exports = {
